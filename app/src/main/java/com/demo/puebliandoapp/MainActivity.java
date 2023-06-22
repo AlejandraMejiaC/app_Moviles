a1a1ee4df624f622bff9e828c212af08a5ba8821
mariana <mariana.mar170906@gmail.com>
2023-06-21 20:00:28 -0500

    se crea splash

--- app/src/main/java/com/demo/puebliandoapp/MainActivity.java
+++ app/src/main/java/com/demo/puebliandoapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.demo.puebliandoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    //atributos=elementos de la actividad
+    MediaPlayer sonido;
+
+    //metodos
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sonido=MediaPlayer.create(this,R.raw.audioapp);
+        sonido.start();
+
+        //temporizamos el splash
+
+        TimerTask inicioApp=new TimerTask() {
+            @Override
+            public void run() {
+                //lanzo el home
+                Intent lanzamiento=new Intent(MainActivity.this,Home.class);
+                startActivity(lanzamiento);
+            }
+        };
+
+        Timer tiempo=new Timer();
+        tiempo.schedule(inicioApp,10000);
     }
 }
